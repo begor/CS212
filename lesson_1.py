e2d375c97d1ee33e862b44773ea5ef766e9f3595
Biriukov Egor <birykovegor@gmail.com>
2016-05-20 12:49:54 +0300

    Tests for card_ranks

--- lesson_1.py
+++ lesson_1.py
@@ -34,6 +34,9 @@ def test():
     sf = "6C 7C 8C 9C TC".split()
     fk = "9D 9H 9S 9C 7D".split()
     fh = "TD TC TH 7C 7D".split()
+    assert card_ranks(sf) == [10, 9, 8, 7, 6]
+    assert card_ranks(fk) == [9, 9, 9, 9, 7]
+    assert card_ranks(fh) == [10, 10, 10, 7, 7]
     assert poker([sf, fk, fh]) == sf
     assert poker([fk, fh]) == fk
     assert poker([fh, fh]) == fh
