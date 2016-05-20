bac35b108d28d0d35c4fdec2a3e8026fd4d326b3
Biriukov Egor <birykovegor@gmail.com>
2016-05-20 12:47:32 +0300

    Hand rank procedure

--- lesson_1.py
+++ lesson_1.py
@@ -7,7 +7,26 @@ def poker(hands):
 
 
 def hand_rank(hand):
-    """Return a tuple indicating the ranking of a hand."""
+    """Return tuple indicating a value of a hand."""
+    ranks = card_ranks(hand)
+    if straight(ranks) and flush(hand):            # straight flush
+        return (8, max(ranks))
+    elif kind(4, ranks):                           # 4 of a kind
+        return (7, kind(4, ranks), kind(1, ranks))
+    elif kind(3, ranks) and kind(2, ranks):        # full house
+        return (6, kind(3, ranks), kind(2, ranks))
+    elif flush(hand):                              # flush
+        return (5, ranks)
+    elif straight(ranks):                          # straight
+        return (4, max(ranks))
+    elif kind(3, ranks):                           # 3 of a kind
+        return (3, kind(3, ranks), ranks)
+    elif two_pair(ranks):                          # 2 pair
+        return (2, two_pair(ranks), ranks)
+    elif kind(2, ranks):                           # kind
+        return (1, kind(2, ranks), ranks)
+    else:                                          # high card
+        return (0, ranks)
 
 
 def test():
