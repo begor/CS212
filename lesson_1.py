0b0cc56c50344be518210dd4f8a026e94c8345cd
Biriukov Egor <birykovegor@gmail.com>
2016-05-20 12:06:18 +0300

    More extreme tests.

--- lesson_1.py
+++ lesson_1.py
@@ -9,12 +9,14 @@ def hand_rank(hand):
 
 def test():
     "Test cases for the functions in poker program"
-    sf = "6C 7C 8C 9C TC".split()  # => ['6C', '7C', '8C', '9C', 'TC']
+    sf = "6C 7C 8C 9C TC".split()
     fk = "9D 9H 9S 9C 7D".split()
     fh = "TD TC TH 7C 7D".split()
     assert poker([sf, fk, fh]) == sf
     assert poker([fk, fh]) == fk
     assert poker([fh, fh]) == fh
+    assert poker([fh]) == fh
+    assert poker(100 * [fh]) == fh
     return 'tests passed'
 
 
