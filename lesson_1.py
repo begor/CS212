45f30d795f6fe064a5c6906451e399b67e3898ad
Biriukov Egor <birykovegor@gmail.com>
2016-05-20 12:05:23 +0300

    Tests added.

--- lesson_1.py
+++ lesson_1.py
@@ -5,3 +5,17 @@ def poker(hands):
 
 def hand_rank(hand):
     pass
+
+
+def test():
+    "Test cases for the functions in poker program"
+    sf = "6C 7C 8C 9C TC".split()  # => ['6C', '7C', '8C', '9C', 'TC']
+    fk = "9D 9H 9S 9C 7D".split()
+    fh = "TD TC TH 7C 7D".split()
+    assert poker([sf, fk, fh]) == sf
+    assert poker([fk, fh]) == fk
+    assert poker([fh, fh]) == fh
+    return 'tests passed'
+
+
+print(test())
