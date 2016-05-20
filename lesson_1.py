8b198c8cf95139c871aa1300187a9b1ac9fa1e31
Biriukov Egor <birykovegor@gmail.com>
2016-05-20 12:31:38 +0300

    Tests for hand_rank

--- lesson_1.py
+++ lesson_1.py
@@ -1,14 +1,17 @@
+"""Lesson 1. Poker game function."""
+
+
 def poker(hands):
-    """Return the best hand"""
+    """Return the best hand."""
     return max(hands, key=hand_rank)
 
 
 def hand_rank(hand):
-    pass
+    """Return a tuple indicating the ranking of a hand."""
 
 
 def test():
-    "Test cases for the functions in poker program"
+    """Test cases for the functions in poker program."""
     sf = "6C 7C 8C 9C TC".split()
     fk = "9D 9H 9S 9C 7D".split()
     fh = "TD TC TH 7C 7D".split()
@@ -17,6 +20,9 @@ def test():
     assert poker([fh, fh]) == fh
     assert poker([fh]) == fh
     assert poker(100 * [fh]) == fh
+    assert hand_rank(sf) == (8, 10)
+    assert hand_rank(fk) == (7, 9, 7)
+    assert hand_rank(fh) == (6, 10, 7)
     return 'tests passed'
 
 
