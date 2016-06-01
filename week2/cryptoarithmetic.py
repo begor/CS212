b7e4bd4717253c608c3dc42bcf982dc0ec66c4de
Biriukov Egor <birykovegor@gmail.com>
2016-06-01 21:57:25 +0300

    Cryptoarithmetic solve procedure.

---
+++ week2/cryptoarithmetic.py
@@ -0,0 +1,19 @@
+import string
+import re
+
+
+def solve(formula):
+    """Given a formula like 'ODD + ODD == EVEN', fill in digits to solve it.
+    Input formula is a string; output is a digit-filled-in string or None."""
+    for variant in fill_in(formula):
+        if valid(variant):
+            return variant
+
+
+def valid(f):
+    """Formula f is valid if and only if it has no
+    numbers with leading zero, and evals true."""
+    try:
+        return not re.search(r'\b0[0-9]', f) and eval(f) is True
+    except ArithmeticError:
+        return False
