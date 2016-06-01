cfd029051be18cea10bb358ecd52928ad2be249d
Biriukov Egor <birykovegor@gmail.com>
2016-06-01 22:11:35 +0300

    Fill in procedure.

--- week2/cryptoarithmetic.py
+++ week2/cryptoarithmetic.py
@@ -1,13 +1,22 @@
 import string
 import re
+import itertools
 
 
 def solve(formula):
     """Given a formula like 'ODD + ODD == EVEN', fill in digits to solve it.
     Input formula is a string; output is a digit-filled-in string or None."""
-    for variant in fill_in(formula):
-        if valid(variant):
-            return variant
+    for f in fill_in(formula):
+        if valid(f):
+            return f
+
+
+def fill_in(formula):
+    "Generate all possible fillings-in of letters in formula with digits."
+    letters = ''.join(s for s in formula if s.isalpha())
+    for digits in itertools.permutations('1234567890', len(letters)):
+        table = str.maketrans(letters, ''.join(digits))
+        yield formula.translate(table)
 
 
 def valid(f):
@@ -17,3 +26,5 @@ def valid(f):
         return not re.search(r'\b0[0-9]', f) and eval(f) is True
     except ArithmeticError:
         return False
+
+print(solve('ODD + ODD == EVEN'))
