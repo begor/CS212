111eab5f1afc3e873f5446a25744459a5d7aa69e
Biriukov Egor <birykovegor@gmail.com>
2016-06-07 21:44:30 +0300

    match1 and match_star functions.

--- week3/regexp.py
+++ week3/regexp.py
@@ -25,3 +25,15 @@ def match(pattern, text):
     else:
         return (match1(pattern[0], text) and
                 match(pattern[1:], text[1:]))
+
+
+def match1(p, text):
+    """Return True if first character of text matches pattern p."""
+    if not text:
+        return False
+    return p == '.' or p == text[0]
+
+
+def match_star(p, pattern, text):
+    """Return True if any number of p, followed by pattern matches the text."""
+    return match(pattern, text) or (match1(p, text) and match_star(p, pattern, text[1:]))
