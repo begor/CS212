863a06d377c91731cb8d0c41a92a25e59ac4bbe9
Biriukov Egor <birykovegor@gmail.com>
2016-06-07 21:32:01 +0300

    Match function first cases.

--- week3/regexp.py
+++ week3/regexp.py
@@ -1,6 +1,20 @@
 def search(pattern, text):
     """Return true if pattern appears anywhere in text."""
     if pattern.startswith('^'):
-        return match(pattern, text) # fill this line
+        return match(pattern[1:], text) # fill this line
     else:
         return match('.*' + pattern, text) # fill this line
+
+
+def match(pattern, text):
+    """
+    Return True if pattern appears at the start of text."""
+
+    if pattern == '':
+        return True
+    elif pattern == '$':
+        return text is ''
+    elif len(pattern) > 1 and pattern[1] in '*?':
+    	return True
+    else:
+        return True
