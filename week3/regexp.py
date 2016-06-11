4c65ce6c185b4233720e605b6456ea3aacb1d753
Biriukov Egor <birykovegor@gmail.com>
2016-06-11 13:28:32 +0300

    Small fixes

--- week3/regexp.py
+++ week3/regexp.py
@@ -1,18 +1,19 @@
+
 def search(pattern, text):
     """Return true if pattern appears anywhere in text."""
     if pattern.startswith('^'):
-        return match(pattern[1:], text) # fill this line
+        return match(pattern[1:], text)
     else:
-        return match('.*' + pattern, text) # fill this line
+        return match('.*' + pattern, text)
 
 
 def match(pattern, text):
     """Return True if pattern appears at the start of text."""
 
     if pattern == '':
-        return True
+        return True  # empty string occures in every string
     elif pattern == '$':
-        return (text == '')
+        return text == ''
     elif len(pattern) > 1 and pattern[1] in '*?':
         p, op, pat = pattern[0], pattern[1], pattern[2:]
         if op == '*':
@@ -36,4 +37,5 @@ def match1(p, text):
 
 def match_star(p, pattern, text):
     """Return True if any number of p, followed by pattern matches the text."""
-    return match(pattern, text) or (match1(p, text) and match_star(p, pattern, text[1:]))
+    return (match(pattern, text) or
+            (match1(p, text) and match_star(p, pattern, text[1:])))
