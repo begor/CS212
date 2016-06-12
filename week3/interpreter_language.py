05ba25f5fb918a8c81e78a1c9c8f245b46c6160c
Biriukov Egor <birykovegor@gmail.com>
2016-06-12 14:48:59 +0300

    Add n_ary decorator

--- week3/interpreter_language.py
+++ week3/interpreter_language.py
@@ -1,3 +1,6 @@
+from functools import wraps
+
+
 def search(pattern, text):
     "Match pattern anywhere in text; return longest earliest match or None."
     for i in range(len(text)):
@@ -51,14 +54,25 @@ def matchset(pattern, text):
 null = frozenset()
 
 
+def n_ary(f):
+    """Given binary function f(x, y), return an n_ary function such
+    that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x."""
+    @wraps(f)
+    def n_ary_f(x, *args):
+        return x if not args else f(x, n_ary_f(*args))
+    return n_ary_f
+
+
 def lit(string):
     return ('lit', string)
 
 
+@n_ary
 def seq(x, y):
     return ('seq', x, y)
 
 
+@n_ary
 def alt(x, y):
     return ('alt', x, y)
 
@@ -78,16 +92,9 @@ def opt(x):
 def oneof(chars):
     return ('oneof', tuple(chars))
 
+
 dot = ('dot',)
 eol = ('eol',)
-
-
-def n_ary(f):
-    """Given binary function f(x, y), return an n_ary function such
-    that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x."""
-    def n_ary_f(x, *args):
-        return x if not args else f(x, n_ary_f(*args))
-    return n_ary_f
 
 
 def test():
