4dac52f899814d1038b64ca05973c6b79f969265
Biriukov Egor <birykovegor@gmail.com>
2016-06-12 18:38:14 +0300

    Tools package with decorators

--- week3/interpreter_language.py
+++ week3/interpreter_language.py
@@ -1,4 +1,4 @@
-from functools import wraps
+from tools import n_ary
 
 
 def search(pattern, text):
@@ -52,15 +52,6 @@ def matchset(pattern, text):
         raise ValueError('unknown pattern: %s' % pattern)
 
 null = frozenset()
-
-
-def n_ary(f):
-    """Given binary function f(x, y), return an n_ary function such
-    that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x."""
-    @wraps(f)
-    def n_ary_f(x, *args):
-        return x if not args else f(x, n_ary_f(*args))
-    return n_ary_f
 
 
 def lit(string):
---
+++ week3/tools.py
@@ -0,0 +1,18 @@
+from functools import wraps, update_wrapper
+
+
+def decorator(d):
+    """Makes function a decorator by copying its inner attribs."""
+    return lambda fn: update_wrapper(d(fn), fn)
+
+
+decorator = decorator(decorator)
+
+
+def n_ary(f):
+    """Given binary function f(x, y), return an n_ary function such
+    that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x."""
+    @wraps(f)
+    def n_ary_f(x, *args):
+        return x if not args else f(x, n_ary_f(*args))
+    return n_ary_f
