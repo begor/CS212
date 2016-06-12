24e2b4441416d796820e5d92788d3a0a7048c6fc
Biriukov Egor <birykovegor@gmail.com>
2016-06-12 21:08:46 +0300

    Memoization decorator

--- week3/tools.py
+++ week3/tools.py
@@ -9,6 +9,7 @@ def decorator(d):
 decorator = decorator(decorator)
 
 
+@decorator
 def n_ary(f):
     """Given binary function f(x, y), return an n_ary function such
     that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x."""
@@ -16,3 +17,19 @@ def n_ary(f):
     def n_ary_f(x, *args):
         return x if not args else f(x, n_ary_f(*args))
     return n_ary_f
+
+
+@decorator
+def memo(fn):
+    """Memoization decorator."""
+    cache = {}
+
+    def _f(*args):
+        try:
+            return cache[args]
+        except KeyError:
+            cache[args] = result = fn(*args)
+            return result
+        except TypeError:  # in the case that args is unhashable
+            return _f(*args)
+    return _f
