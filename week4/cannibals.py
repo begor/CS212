cd6950ba5898b18e971896918725e48dc072c5b6
Biriukov Egor <birykovegor@gmail.com>
2016-07-10 16:24:50 +0300

    Teacher's solution.

--- week4/cannibals.py
+++ week4/cannibals.py
@@ -15,15 +15,43 @@
 # We should generate successor states that include more cannibals than
 # missionaries, but such a state should generate no successors.
 
-
 def csuccessors(state):
+
+    def add(X, Y):
+      return tuple(x+y for x, y in zip(X, Y))
+
+    def sub(X, Y):
+      return tuple(x-y for x, y in zip(X, Y))
+
+    DELTAS = {(2, 0, 1, -2, 0, -1): 'MM',
+              (0, 2, 1, 0, -2, -1): 'CC',
+              (1, 1, 1, -1, -1, -1): 'MC',
+              (1, 0, 1, -1, 0, -1): 'M',
+              (0, 1, 1, 0, -1, -1): 'C'}
+
+    M1, C1, B1, M2, C2, B2 = state
+    if M1 < C1 or M2 < C2:
+      return {}
+
+    items = []
+    if B1 > 0:
+      items += [(sub(state, delta), action + '->')
+                for delta, action in DELTAS.items()]
+    if B2 > 0:
+      items += [(add(state, delta), '<-' + action)
+                for delta, action in DELTAS.items()]
+
+    return dict(items)
+
+
+
+def csuccessors_mine(state):
     """Find successors (including those that result in dining) to this
     state. But a state where the cannibals can dine has no successors."""
 
     M1, C1, B1, M2, C2, B2 = state
-    result = {}
     if M1 < C1 or M2 < C2:
-      return result
+      return {}
 
     if B1 > 0:
       return {((M1-m), (C1-c), B1-1, M2+m, C2+c, B2+1): 'M' * m + 'C' * c + '->'
@@ -38,18 +66,19 @@ def csuccessors(state):
 
 
 
-def test():
-    assert csuccessors((2, 2, 1, 0, 0, 0)) == {(2, 1, 0, 0, 1, 1): 'C->',
+def test(func):
+    assert func((2, 2, 1, 0, 0, 0)) == {(2, 1, 0, 0, 1, 1): 'C->',
                                                (1, 2, 0, 1, 0, 1): 'M->',
                                                (0, 2, 0, 2, 0, 1): 'MM->',
                                                (1, 1, 0, 1, 1, 1): 'MC->',
                                                (2, 0, 0, 0, 2, 1): 'CC->'}
-    assert csuccessors((1, 1, 0, 4, 3, 1)) == {(1, 2, 1, 4, 2, 0): '<-C',
+    assert func((1, 1, 0, 4, 3, 1)) == {(1, 2, 1, 4, 2, 0): '<-C',
                                                (2, 1, 1, 3, 3, 0): '<-M',
                                                (3, 1, 1, 2, 3, 0): '<-MM',
                                                (1, 3, 1, 4, 1, 0): '<-CC',
                                                (2, 2, 1, 3, 2, 0): '<-MC'}
-    assert csuccessors((1, 4, 1, 2, 2, 0)) == {}
+    assert func((1, 4, 1, 2, 2, 0)) == {}
     return 'tests pass'
 
-print(test())
+print(test(csuccessors_mine))
+print(test(csuccessors))
